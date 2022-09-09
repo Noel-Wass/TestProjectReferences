6879d2892965f91cad4cb55d4d63b62642dee12a
Noel Wass <focusonfundraising@xtra.co.nz>
2022-09-09 14:54:11 +1200

    resolve-paths-02/test.

---
+++ sample-library/dist/resolve-paths-02/test.d.ts
@@ -0,0 +1,6 @@
+declare function test1(): Promise<void>;
+declare function test2(): Promise<void>;
+declare function test3(): Promise<void>;
+declare function test4(): Promise<void>;
+declare function test5(): Promise<void>;
+declare function test6(): Promise<void>;
